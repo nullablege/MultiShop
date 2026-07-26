863a6eb50c5f1a7f81f32db4c489684333ccc5f4
Ege <nullablege@users.noreply.github.com>
2026-07-26 17:45:16 +0300

    Order EF Core context altyapısını ekle

---
+++ Services/Order/Infrastructure/MultiShop.Order.Persistence/Context/OrderDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Persistence.Context
+{
+    public class OrderDbContext : DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Address> Addresses { get; set; } = null!;
+        public DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+        public DbSet<Ordering> Orderings { get; set; } = null!;
+    }
+}
