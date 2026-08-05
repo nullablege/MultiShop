10c8cb689e62869fac7ade45dfa1ce87f369417f
Ege <nullablege@users.noreply.github.com>
2026-08-05 19:08:41 +0300

    Admin alanı ve kategori hazırlığını oluştur

--- Frontends/MultiShop.WebUI/Program.cs
+++ Frontends/MultiShop.WebUI/Program.cs
@@ -16,6 +16,11 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.MapAreaControllerRoute(
+    name: "admin",
+    areaName: "Admin",
+    pattern: "Admin/{controller=Dashboard}/{action=Index}/{id?}");
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
