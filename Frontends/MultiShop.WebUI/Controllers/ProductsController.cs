a71c668db77da743b6e3df405646347d836fe89f
Ege <nullablege@users.noreply.github.com>
2026-08-05 18:58:52 +0300

    WebUI ürün listesi tasarımını ekle

---
+++ Frontends/MultiShop.WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers;
+
+public class ProductsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
