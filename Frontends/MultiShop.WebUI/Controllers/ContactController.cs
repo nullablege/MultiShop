8756c992c87409d029cb9e10186946675fe99ad8
Ege <nullablege@users.noreply.github.com>
2026-08-05 19:03:41 +0300

    WebUI sepet ve iletişim sayfalarını ekle

---
+++ Frontends/MultiShop.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers;
+
+public class ContactController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
