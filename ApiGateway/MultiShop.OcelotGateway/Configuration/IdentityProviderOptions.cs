12a8a20368af2ce86ec8d4d07c64230fc79ec528
Ege <nullablege@users.noreply.github.com>
2026-08-18 17:32:13 +0300

    Gateway rotalarını scope ile koru

---
+++ ApiGateway/MultiShop.OcelotGateway/Configuration/IdentityProviderOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace MultiShop.OcelotGateway.Configuration
+{
+    public class IdentityProviderOptions
+    {
+        public const string SectionName = "IdentityProvider";
+        public string Issuer { get; init; } = string.Empty;
+    }
+}
--- ApiGateway/MultiShop.OcelotGateway/Program.cs
+++ ApiGateway/MultiShop.OcelotGateway/Program.cs
@@ -1,5 +1,7 @@
+using MultiShop.OcelotGateway.Configuration;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using OpenIddict.Validation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +9,48 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
                      .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true);
 
+
+builder.Services.AddOptions<IdentityProviderOptions>()
+    .BindConfiguration(IdentityProviderOptions.SectionName)
+    .Validate(options =>
+    {
+        return Uri.TryCreate(options.Issuer, UriKind.Absolute, out var issueUri) && issueUri.Scheme == Uri.UriSchemeHttps;
+    }, "IdentityProvider:Issuer geçerli bir HTTPS adresi olmalýdýr. ")
+    .ValidateOnStart();
+
+var identityProviderOptions = builder.Configuration
+    .GetRequiredSection(IdentityProviderOptions.SectionName)
+    .Get<IdentityProviderOptions>() ?? throw new InvalidOperationException("IdentityProvider yapýlandýrmasý bulunamadý");
+
+builder.Services.AddOpenIddict()
+    .AddValidation(options =>
+    {
+        options.SetIssuer(new Uri(identityProviderOptions.Issuer));
+
+        options.AddAudiences(
+            "catalog_api",
+            "discount_api",
+            "order_api",
+            "cargo_api",
+            "basket_api",
+            "comment_api"
+            );
+
+        options.UseSystemNetHttp();
+        options.UseAspNetCore();
+    });
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme;
+});
+
+
+
+builder.Services.AddAuthorization();
+
 builder.Services.AddOcelot(builder.Configuration);
+
 
 var app = builder.Build();
 
